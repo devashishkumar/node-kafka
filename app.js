e0f3a4c8e46d5a08e952e64f6ae2298c7f097322
= <=>
2021-05-10 16:40:54 +0530

    add code to get all the topics using kafkajs

--- app.js
+++ app.js
@@ -8,32 +8,29 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 7);
+}
+// kafka configuration start
 const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'testapp',
   // brokers: ['kafka1:9092', 'kafka2:9092']
   // brokers: ['DESKTOP-LKNKAGN.mshome.net:2181']
-  brokers: ['DESKTOP-LKNKAGN.mshome.net:9092']
+  brokers: ['DESKTOP-LKNKAGN:9092']
 });
+// kafka configuration end
 
-// const producer = kafka.producer()
-
-// producer.connect();
-// producer.send({
-//   topic: 'test',
-//   messages: [
-//     { value: 'Hello KafkaJS user!' },
-//   ],
-// });
-
+// kafka producer start
 const producer = kafka.producer();
+let newTopic = 'test';
 setInterval(() => {
+  // newTopic = generateRandomString();
   var sendMessage = async () => {
     await producer.connect()
     await producer.send({
-      topic: 'test',
+      topic: 'testnew',
       messages: [
         { key: 'name', value: 'Ashish K' }
       ],
@@ -43,22 +40,45 @@ setInterval(() => {
 
   sendMessage();
 }, 10000);
+// kafka producer end
 
+// kafka subscriber start
 const consumer = kafka.consumer({ groupId: 'testapp' })
 
 var receiveMessage = async () => {
-  await consumer.connect()
-  await consumer.subscribe({ topic: 'test', fromBeginning: true })
+  try {
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'testnew', fromBeginning: true })
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        value: message.value.toString(),
-      })
-    },
-  })
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          value: message.value.toString(),
+        })
+      },
+    })
+  } catch (e) {
+    console.log('error');
+  }
+
 };
+// kafka subscriber end
 receiveMessage();
+
+// get all topics start
+setInterval(() => {
+  const admin = kafka.admin()
+  var getAllTopics = async () => {
+    await admin.connect();
+    const topics = await admin.listTopics();
+    return topics;
+  };
+  return  getAllTopics().then(allTopics => {
+    console.log(allTopics, '79');
+  })
+}, 5000);
+// get all topics end
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
